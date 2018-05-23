7625b1d0f8feb2d2cfa55446e22158550eef7876
gr33ngr33n <actfun07@gmail.com>
2018-05-24 00:04:39 +0800

    created separated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  servers: { name: string, type: string, content: string }[] = [
+    {
+    name: 'server1',
+    type: 'Server',
+    content: 'server1 content'
+    },
+    {
+      name: 'server2',
+      type: 'Blueprint',
+      content: 'server2 content'
+    }
+  ];
+
+  onElementDelete (id: number) {
+    this.servers.splice(id, 1);
+  }
+
+  onServerAdd (serverData: {sName: string, sContent: string}) {
+    this.servers.push(
+      {
+        name: serverData.sName,
+        type: 'Server',
+        content: serverData.sContent
+      }
+    );
+  }
+
+  onBlueprintAdd (blueprintData: {sName: string, sContent: string}) {
+    this.servers.push(
+      {
+        name: blueprintData.sName,
+        type: 'Blueprint',
+        content: blueprintData.sContent
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ServerElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
